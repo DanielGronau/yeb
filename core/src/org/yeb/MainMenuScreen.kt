b3114d0a65d02138720201be2bce605c10baf206
DanielGronau <Daniel.Gronau@gmx.de>
2018-04-20 12:19:32 +0200

    kotlinized code

---
+++ core/src/org/yeb/MainMenuScreen.kt
@@ -0,0 +1,47 @@
+package org.yeb
+
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.Screen
+import com.badlogic.gdx.graphics.GL20
+import com.badlogic.gdx.graphics.OrthographicCamera
+
+class MainMenuScreen(internal val game: YebGame) : Screen {
+    internal var camera: OrthographicCamera
+
+    init {
+
+        camera = OrthographicCamera()
+        camera.setToOrtho(false, 800f, 480f)
+
+    }
+
+    override fun render(delta: Float) {
+        Gdx.gl.glClearColor(0f, 0f, 0.2f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+
+        camera.update()
+        game.batch.projectionMatrix = camera.combined
+
+        game.batch.begin()
+        game.font.draw(game.batch, "Welcome to Yeb!!! ", 100f, 150f)
+        game.font.draw(game.batch, "Tap anywhere to begin!", 100f, 100f)
+        game.batch.end()
+
+        if (Gdx.input.isTouched) {
+            game.screen = GameScreen(game)
+            dispose()
+        }
+    }
+
+    override fun resize(width: Int, height: Int) {}
+
+    override fun show() {}
+
+    override fun hide() {}
+
+    override fun pause() {}
+
+    override fun resume() {}
+
+    override fun dispose() {}
+}
---
+++ desktop/src/org/yeb/desktop/DesktopLauncher.kt
@@ -0,0 +1,13 @@
+package org.yeb.desktop
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
+import org.yeb.YebGame
+
+object DesktopLauncher {
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = LwjglApplicationConfiguration()
+        LwjglApplication(YebGame(), config)
+    }
+}
